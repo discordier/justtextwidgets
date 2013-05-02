e6ed7c9825001b7eb10c76f3cf8e5329cf35de70
Andreas Isaak <andy.jared@googlemail.com>
2013-05-03 00:42:44 +0200

    Change copyright, add autoload.php and composer.json

--- src/system/modules/justtextwidgets/config/config.php
+++ src/system/modules/justtextwidgets/config/config.php
@@ -1,10 +1,6 @@
 <?php
+
 /**
- * The MetaModels extension allows the creation of multiple collections of custom items,
- * each with its own unique set of selectable attributes, with attribute extendability.
- * The Front-End modules allow you to build powerful listing and filtering of the
- * data in each collection.
- *
  * PHP version 5
  * @package	   JustTextWidgets
  * @subpackage Core
@@ -13,10 +9,6 @@
  * @license    LGPL
  * @filesource
  */
-if (!defined('TL_ROOT'))
-{
-	die('You cannot access this file directly!');
-}
 
 /**
  * Form fields
@@ -25,5 +17,3 @@ $GLOBALS['BE_FFL']['justtext']  = 'JustAText';
 $GLOBALS['BE_FFL']['justtextoption']  = 'JustATextOption';
 $GLOBALS['BE_FFL']['justexplanation'] = 'JustAnExplanation';
 $GLOBALS['BE_FFL']['justsmalltext'] = 'JustASmallText';
-
-?>
