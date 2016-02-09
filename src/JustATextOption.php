1ffd63f1a5763d080ac0c4ea84e5933a5f55725f
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-02-09 14:00:41 +0100

    Add compatibility fixes for new composer-plugin
    - Bump phpcq to 1.1
    - Fix code style
    - Add LICENSE file
    - Bump composer plugin version to at least 2.4

--- src/JustATextOption.php
+++ src/JustATextOption.php
@@ -1,12 +1,21 @@
 <?php
 
 /**
+ * This file is part of discordier/justtextwidgets.
+ *
+ * (c) 2012-2016 CyberSpectrum
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
  * @package    JustTextWidgets
  * @subpackage Core
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
  * @author     Andreas Isaak <andy.jared@googlemail.com>
- * @copyright  CyberSpectrum
- * @license    LGPL
+ * @copyright  2012-2016 CyberSpectrum
+ * @license    https://github.com/discordier/justtextwidgets/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
 
@@ -15,10 +24,6 @@ namespace Discordier\JustTextWidgets;
 /**
  * Display a hidden field with a fixed value in the backend and the option name next to it.
  * This is useful when you need predefined values in a MultiColumnWizard i.e.
- *
- * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  CyberSpectrum
- * @package    JustTextWidgets
  */
 class JustATextOption extends \Widget
 {
